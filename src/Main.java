a22d5e4d660513cbe857d8bf04b5a18cf02b8639
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2025-12-15 17:05:24 +0000

    add input validation

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         //Variables
         final int MONTHS_IN_YEAR = 12;
         final int PERCENT = 100;
-        int principle;
+        int principal;
         int years;
         int numberOfPayments;
         double mortgage;
         double annualInterest;
         double monthlyInterest;
-        String mortgageOutput;
+        String mortgageOutput ;
         Scanner scanner = new Scanner(System.in);
 
         //Display welcome
@@ -22,31 +22,57 @@ public class Main {
         System.out.println("Mortgage Calculator");
         System.out.println("====================");
 
-        //Get user input
-        System.out.print("Enter Principle amount: ");
-        principle = scanner.nextInt();
+        //Get input
+        while(true){
+            System.out.print("Enter Principal amount ($1K - $1M): ");
+            principal = scanner.nextInt();
 
-        System.out.print("Enter Annual Interest Rate: ");
-        annualInterest = scanner.nextDouble();
-        monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            //validation check
+            if(principal >= 100 && principal <= 1_000_000){
+                break;
+            }
+            System.out.println("Please enter a value between 1000 and 1000000");
+        }
 
-        System.out.print("Enter Period (Years):  ");
-        years = scanner.nextInt();
+        //Get input
+        while(true){
+            System.out.print("Enter Annual Interest Rate: ");
+            annualInterest = scanner.nextDouble();
+
+            //validation check
+            if(annualInterest >= 1 && annualInterest <= 30){
+                break;
+            }
+            System.out.println("Please enter a value between 1 and 30");
+        }
+
+        //Get input
+        while(true) {
+            System.out.print("Enter Period (Years):  ");
+            years = scanner.nextInt();
+
+            //validation check
+            if(years >= 1 && years <= 35) {
+                break;
+            }
+            System.out.println("Please enter a value between 1 and 30");
+        }
+
+        //Calculations
         numberOfPayments = years * MONTHS_IN_YEAR;
-
-        //Calculate mortgage payments
-        mortgage = principle
+        monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
 
         //Display results
         mortgageOutput = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("====================");
         System.out.println("Mortgage: " + mortgageOutput);
         System.out.println("====================");
 
-
-
-
+        //Closures
+        scanner.close();
 
     }
 }
